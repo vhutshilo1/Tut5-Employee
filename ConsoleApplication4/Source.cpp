5c6cb8ca8e11c0431d80160361a645563fcd5ff0
vhutshilo1 <KharivheVhutshilo@live.com>
2016-03-29 08:10:12 +0200

    driver  program

--- ConsoleApplication4/Source.cpp
+++ ConsoleApplication4/Source.cpp
@@ -135,3 +135,51 @@ public:
 private:
 	float Rate, BaseSalary, Revenue, Earnings;
 };
+//Driver Program
+int main()
+{
+	//declaring pointers to objects
+	Employee *salaryEmpPtr, *hourlyEmpPtr, *commEmpPtr;
+	string temp;
+	//creating objects of each type
+	SalaryEmployee salaryEmployee1("Makepeace", 11);
+	HourlyEmployee hourlyEmployee1("Busisiwe", 12);
+	CommissionEmployee commissionEmployee1("Mukuda", 13);
+	//Assigning addresses of each object to base-pointers
+	salaryEmpPtr = &salaryEmployee1;
+	hourlyEmpPtr = &hourlyEmployee1;
+	commEmpPtr = &commissionEmployee1;
+	//====================================================================
+	//Calculating total wages paid to employees, polymorphically
+	//====================================================================
+
+	cout << "***********CALCULATING TOTAL WAGES POLYMORPHICALLY**********\n" << endl;
+
+
+	//1.Salaried Employee.
+	cout << "STAFF NAME:" << salaryEmployee1.name() << '\n';
+	cout << "STAFF NUMBER:" << salaryEmployee1.staffNumber() << '\n';
+	salaryEmployee1.setSalary(25.0);	//setting salaryEmployee1's salary.
+	cout << "TOTAL WAGE: $"
+		<< salaryEmpPtr->salary() << '\n' << '\n';	//indirect access of base-class member.
+
+													//2.Hourly Emlployee.
+	cout << "STAFF NAME:" << hourlyEmployee1.name() << '\n';
+	cout << "STAFF NUMBER:" << hourlyEmployee1.staffNumber() << '\n';
+	hourlyEmployee1.setHourlyRate(1.50);	//setting salaryEmployee1's rate/hour.
+	hourlyEmployee1.setHoursWorked(10);	//setting salaryEmployee1's hours worked.
+	cout << "TOATAL WAGE: $"
+		<< hourlyEmpPtr->salary() << '\n' << '\n';	//indirect access of base-class member.
+
+													//3.Commission Employee.
+	cout << "STAFF NAME:" << commissionEmployee1.name() << '\n';
+	cout << "STAFF NUMBER:" << commissionEmployee1.staffNumber() << '\n';
+	commissionEmployee1.setBaseSalary(15.5);	//setting salaryEmployee1's base salary.
+	commissionEmployee1.setRate(0.5);	//setting salaryEmployee1's commission %.
+	commissionEmployee1.setRevenue(0.5);	//setting salaryEmployee1's revenue
+	cout << "TOTAL WAGE: $"
+		<< commEmpPtr->salary() << '\n' << '\n';	//indirect access of base-class member.
+
+	return 0;
+
+}
